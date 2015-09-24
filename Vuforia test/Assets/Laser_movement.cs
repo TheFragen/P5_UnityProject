9f1c8f12efa5adc30625e6af7c7f24fd783cf098
Eblermeister <Jonaseblerpedersen@hotmail.com>
2015-09-24 14:22:01 +0200

    I am not nigger

--- Vuforia test/Assets/Laser_movement.cs
+++ Vuforia test/Assets/Laser_movement.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Enemy_movement : MonoBehaviour
+public class Laser_movement : MonoBehaviour
 {
 
     // Use this for initialization
@@ -17,13 +17,13 @@ public class Enemy_movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = (1 - t) * (1 - t) * P0.transform.position + 2 * (1 - t) * t * P1.transform.position + (t * t) * P2.transform.position;
+       /* transform.position = (1 - t) * (1 - t) * P0.transform.position + 2 * (1 - t) * t * P1.transform.position + (t * t) * P2.transform.position;
         t += velocity * Time.deltaTime;
         if (t > 1.0)
         {
             velocity = -velocity;
 
         }
-        if (t < 0.0) { velocity = -velocity; }
+        if (t < 0.0) { velocity = -velocity; }*/
     }
 }
