d92d9242196325ff11c68aff0c5846061c3ac01b
TheFragen <christian.faester@yahoo.dk>
2015-10-15 12:17:05 +0200

    Enemy AI fixes and improvements.

--- Game/Assets/Scripts/EnemyMovementNavAgent.cs
+++ Game/Assets/Scripts/EnemyMovementNavAgent.cs
@@ -3,42 +3,41 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
-public class EnemyMovementNavAgent : MonoBehaviour {
+public class EnemyMovementNavAgent : MonoBehaviour
+{
     public GameObject[] waypoint;
     private List<GameObject> initialSearch = new List<GameObject>();
     private List<GameObject> sortedSearch = new List<GameObject>();
 
     public float patrolSpeed = 2f;                          // The nav mesh agent's speed when patrolling.
-	public float chaseSpeed = 5f;                           // The nav mesh agent's speed when chasing.
-	public float chaseWaitTime = 5f;                        // The amount of time to wait when the last sighting is reached.
-	public float patrolWaitTime = 1f;                       // The amount of time to wait when the patrol way point is reached.
+    public float chaseSpeed = 5f;                           // The nav mesh agent's speed when chasing.
+    public float chaseWaitTime = 5f;                        // The amount of time to wait when the last sighting is reached.
+    public float patrolWaitTime = 1f;                       // The amount of time to wait when the patrol way point is reached.
 
-	private NavMeshAgent agent;
-	private EnemySight enemySight;
+    private NavMeshAgent agent;
+    private EnemySight enemySight;
 
-	private Transform player;
+    private Transform player;
 
-	private float chaseTimer;                               // A timer for the chaseWaitTime.
-	private float patrolTimer;                              // A timer for the patrolWaitTime.
-	private int pathPointIndex;
+    private float chaseTimer;                               // A timer for the chaseWaitTime.
+    private float patrolTimer;                              // A timer for the patrolWaitTime.
+    private int pathPointIndex;
 
     private bool recheck = false;
     private Quaternion target;
 
     // Use this for initialization
-    void Start () {
-		agent = GetComponent<NavMeshAgent> ();
-		agent.stoppingDistance = 2.0f;
+    void Start() {
+        agent = GetComponent<NavMeshAgent>();
+        agent.stoppingDistance = 1.0f;
 
-		enemySight = GetComponent<EnemySight> ();
-	//	player = GameObject.FindWithTag("Player").transform;
+        enemySight = GetComponent<EnemySight>();
+        //	player = GameObject.FindWithTag("Player").transform;
 
         //Find all the waypoints and sort them by their index
         initialSearch = GameObject.FindGameObjectsWithTag("EnemyWaypoint").ToList();
-        foreach (GameObject elem in initialSearch.ToList())
-        {
-            if (elem.transform.parent == this.transform)
-            {
+        foreach (GameObject elem in initialSearch.ToList()) {
+            if (elem.transform.parent == this.transform) {
                 sortedSearch.Add(elem);
             }
         }
@@ -46,84 +45,89 @@ public class EnemyMovementNavAgent : MonoBehaviour {
         initialSearch = null;
         sortedSearch = null;
         target = Quaternion.Euler(0, 180, 0);
-    }
-	
-	// Update is called once per frame
-	void Update () {
-			
-		if (enemySight.personalLastSighting != enemySight.resestSight) {
-			chase ();
-		} else {
-			patrol ();
-		}
 
-        if (recheck)
-        {
+        agent.destination = waypoint[0].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (enemySight.personalLastSighting != enemySight.resetSight) {
+            chase();
+        } else {
+            patrol();
+        }
+
+        if (recheck) {
             transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * 1.5f);
         }
 
-	}
+    }
 
-	void chase(){
-		agent.speed = chaseSpeed;
+    void chase() {
+        agent.speed = chaseSpeed;
+        agent.destination = enemySight.personalLastSighting;
 
-		agent.destination = enemySight.personalLastSighting;
+        // If near the last personal sighting...
+        if (Vector3.Distance(this.transform.position, agent.destination) < 1.1f) {
+            // Incerement timer
+            chaseTimer += Time.deltaTime;
 
-		// If near the last personal sighting...
-		if(agent.remainingDistance < agent.stoppingDistance)
-		{
-			// ... increment the timer.
-			chaseTimer += Time.deltaTime;
-			
-			// If the timer exceeds the wait time...
-			if(chaseTimer >= chaseWaitTime/3)
-			{
+            // If the timer exceeds the wait time...
+            if (chaseTimer >= chaseWaitTime) {
                 recheck = true;
-                Debug.Log("Waiting");
+
                 //Wait a bit before resetting
-                if(chaseTimer >= chaseWaitTime + 3f)
-                {
-                    enemySight.personalLastSighting = enemySight.resestSight;
+                if (chaseTimer >= chaseWaitTime + 3f) {
+                    Debug.Log("Resetting");
+                    enemySight.personalLastSighting = enemySight.resetSight;
+                    agent.destination = waypoint[pathPointIndex].transform.position;
                     chaseTimer = 0f;
                     recheck = false;
                 }
             }
-		}
-		else
-			// If not near the last sighting personal sighting of the player, reset the timer.
-			chaseTimer = 0f;
-	}
+        } else {
+            // If not near the last sighting personal sighting of the player, reset the timer.
+            chaseTimer = 0f;
+        }
+    }
 
-	void patrol (){
-		// Set an appropriate speed for the NavMeshAgent.
-		agent.speed = patrolSpeed;
+    void patrol() {
+        // Set an appropriate speed for the NavMeshAgent.
+        agent.speed = patrolSpeed;
 
+        //Draw path to next waypoint
+        this.transform.GetComponent<LineRenderer>().SetVertexCount(agent.path.corners.Length);
+        this.transform.GetComponent<LineRenderer>().SetPosition(0, this.transform.position);
+        for (int i = 1; i < agent.path.corners.Length; i++) {
+            this.transform.GetComponent<LineRenderer>().SetPosition(i, agent.path.corners[i] + transform.up / 6);
+        }
 
-		// If near the next waypoint or there is no destination...
-		if(Vector3.Distance(this.transform.position, waypoint[pathPointIndex].transform.position) < 2f) 
-		{
-			// ... increment the timer.
-			patrolTimer += Time.deltaTime;
-			
-			// If the timer exceeds the wait time...
-			if(patrolTimer >= patrolWaitTime)
-			{
-				// ... increment the wayPointIndex.
-				if(pathPointIndex == waypoint.Length - 1)
-					pathPointIndex = 0;
-				else
-					pathPointIndex++;
-				
-				// Reset the timer.
-				patrolTimer = 0;
-			}
-		}
-		else
-			// If not near a destination, reset the timer.
-			patrolTimer = 0;
-		
-		// Set the destination to the patrolWayPoint.
-		agent.destination = waypoint[pathPointIndex].transform.position;
+        // If near the next waypoint or there is no destination...
+        if (Vector3.Distance(this.transform.position, waypoint[pathPointIndex].transform.position) < 1.1f) {
+            // ... increment the timer.
+            patrolTimer += Time.deltaTime;
 
-	}
+            // If the timer exceeds the wait time...
+            if (patrolTimer >= patrolWaitTime) {
+
+                // Increment the wayPointIndex.
+                if (pathPointIndex == waypoint.Length - 1) {
+                    pathPointIndex = 0;
+                } else {
+                    pathPointIndex++;
+                }
+
+                // Reset the timer.
+                patrolTimer = 0;
+            }
+
+            // Set the destination to the patrolWayPoint.
+            agent.destination = waypoint[pathPointIndex].transform.position;
+
+        } else {
+            // If not near a destination, reset the timer.
+            patrolTimer = 0;
+        }
+    }
 }
--- Game/Assets/Scripts/EnemySight.cs
+++ Game/Assets/Scripts/EnemySight.cs
@@ -7,7 +7,7 @@ public class EnemySight : MonoBehaviour {
     public bool playerInSight;
 	public Vector3 personalLastSighting = new Vector3(1000.0f, 1000.0f, 1000.0f);
 
-	public Vector3 resestSight = new Vector3(1000.0f, 1000.0f, 1000.0f);
+	public Vector3 resetSight = new Vector3(1000.0f, 1000.0f, 1000.0f);
     private SphereCollider col;
     private GameObject player;
     private Vector3 previousSighting;
@@ -27,43 +27,39 @@ public class EnemySight : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		// Draw field of view with line renderer
-		/*Quaternion left = Quaternion.AngleAxis ((fovAngle-180) / 2, transform.up);
-		Quaternion right = Quaternion.AngleAxis (180-fovAngle, transform.up);
-		
-		this.GetComponent<LineRenderer>().SetPosition(0,transform.position);
-		this.GetComponent<LineRenderer>().SetPosition(1,(left * this.transform.forward * 4)+transform.position);
-		this.GetComponent<LineRenderer>().SetPosition(2,transform.position);
-		this.GetComponent<LineRenderer>().SetPosition(3,(right * this.transform.forward * 4)+transform.position);
-		*/
 	}
 	
 	void OnTriggerStay (Collider other)
     {
-        if(other.gameObject == player)
+        
+        if (other.gameObject == player)
         {
             playerInSight = false;
 
             Vector3 direction = other.transform.position - transform.position;
             float angle = Vector3.Angle(direction, transform.forward);
 
-            if (angle < fovAngle * 0.5f)
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, direction.normalized, out hit, col.radius * 2))
             {
-                RaycastHit hit;
-                
-                if (Physics.Raycast(transform.position, direction.normalized, out hit, col.radius*2))
+                if (angle < fovAngle * 0.5f)
                 {
                     Debug.DrawLine(transform.position, hit.point, Color.red);
                     if (hit.collider.gameObject == player)
                     {
-                        playerInSight = true;
-                    
+                        playerInSight = true;   
                         personalLastSighting = player.transform.position;
-
                         Debug.Log("player sighted");
                     }
+
+                //Check if player is really close to enemy, and mark that as sight
+                } else if (hit.distance < 1.5f && hit.collider.gameObject == player) {
+                    playerInSight = true;
+                    personalLastSighting = player.transform.position;
+                    Debug.Log("player sighted");
                 }
-            }
+
+            } 
         }
     }
     void OnTriggerExit(Collider other)
