bf6b7007cce9246e91be47677cc3db3de8b265c3
Simon frisendahl petersen <g.t.boss@hotmail.com>
2015-11-30 10:10:18 +0100

    door

--- Game/Assets/Scripts/Door.cs
+++ Game/Assets/Scripts/Door.cs
@@ -3,13 +3,13 @@ using System.Collections;
 
 public class Door : MonoBehaviour
 {
-    public GameObject textObject;
+    public TextMesh textObject;
     public bool locked = true;
     private string[] colors = new string[] { "Red", "Blue", "Green", "Yellow" };
     // Use this for initialization
     void Start()
     {
-        TextMesh textObject = GameObject.Find("ColerCode").GetComponent<TextMesh>();
+        textObject = GameObject.Find("ColerCode").GetComponent<TextMesh>();
 
         for (int i = 0; i < colors.Length; i++)
         {
@@ -20,6 +20,28 @@ public class Door : MonoBehaviour
         }
         
         for(int i = 0; i < colors.Length; i++)
+        {
+            Debug.Log(colors[i]);
+            textObject.text += colors[i] + " ";
+        }
+    }
+    void Update ()
+    {
+
+
+    }
+    void MixOrder()
+    {
+        if (textObject.text != null) textObject.text = null;
+
+        for (int i = 0; i < colors.Length; i++)
+        {
+            string tmp = colors[i];
+            int r = Random.Range(i, colors.Length);
+            colors[i] = colors[r];
+            colors[r] = tmp;
+        }
+        for (int i = 0; i < colors.Length; i++)
         {
             Debug.Log(colors[i]);
             textObject.text += colors[i] + " ";
